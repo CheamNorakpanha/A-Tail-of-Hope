01c336a208784b0e1c23417554786f670004d152
JJ <cheam.panha168@gmail.com>
2025-06-17 23:58:32 +0700

    improve the <Link> components in the header section

--- src/components/header/RightSideButton.tsx
+++ src/components/header/RightSideButton.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-scroll"
 function RightSideButton() {
     return (
         <div className="hidden md:flex items-center">
-            <Link to="get-involved" smooth={true} duration={500} >
+            <Link to="get-involved" smooth={true} duration={500} offset={-65} >
                 <button className="border-yellow-700 bg-yellow-700 text-white hover:bg-yellow-800 px-6 py-2 rounded-md font-medium transition-colors">Get Involved</button>
             </Link>
         </div>
