de5b20e3d4b42a2d8fc4abc11ba51679f7203e69
JJ <cheam.panha168@gmail.com>
2025-06-20 13:47:22 +0700

    fix the <link> of the involved button not working

--- src/components/header/MobileNavigation.tsx
+++ src/components/header/MobileNavigation.tsx
@@ -47,9 +47,12 @@ export default function MobileNavigation() {
                 >
                     Contact
                 </Link>
-                <div className="px-3 py-2">
-                    <button className="w-full border-yellow-700 bg-yellow-700 text-white hover:bg-yellow-800 px-6 py-2 rounded-md font-medium transition-colors">Get Involved</button>
-                </div>
+
+                <Link to="get-involved" smooth={true} duration={500} offset={-360}>
+                    <div className="px-3 py-2">
+                        <button className="w-full border-yellow-700 bg-yellow-700 text-white hover:bg-yellow-800 px-6 py-2 rounded-md font-medium transition-colors">Get Involved</button>
+                    </div>
+                </Link>
             </div>
         </div>
     );
