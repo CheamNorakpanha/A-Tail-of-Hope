35ed1a09baa0882b4ac6565d43e0137be2c4ab4b
JJ <cheam.panha168@gmail.com>
2025-06-24 15:33:05 +0700

    remove flowbite completely

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx,html}",
-    "./node_modules/flowbite-react/**/*.js", // for Flowbite React components
-    "./node_modules/flowbite/**/*.js",        // for base Flowbite components
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
